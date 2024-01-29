fc36b151b3273aa7836f4d267fe11647968c9882
Uliana Dmitrieva <uli.dm13@gmail.com>
2024-01-30 00:13:17 +0100

    Base layout Demo component

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Demo: React.FC = () => {
-  return <div>Demo</div>;
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          // onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browse History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto"></div>
+      </div>
+
+      {/* Display Result */}
+      <div className="my-10 max-w-full flex justify-center items-center"></div>
+    </section>
+  );
 };
 
 export default Demo;
