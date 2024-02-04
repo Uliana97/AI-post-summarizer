afc7c00761b95e9f7404f54540fc1fccf3f4e933
Uliana Dmitrieva <uli.dm13@gmail.com>
2024-02-04 23:02:04 +0100

    complete logic

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -3,23 +3,68 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
+interface Article {
+  url: string;
+  summary: string;
+}
+
 const Demo = () => {
-  const [article, setArticle] = useState({
+  const [article, setArticle] = useState<Article>({
     url: "",
     summary: "",
   });
 
-  const [getSummary, {error, isFetching} ] = useLazyGetSummaryQuery();
+  const [allArticles, setAllArticles] = useState<Article[] | []>([]);
+  const [copied, setCopied] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = localStorage.getItem("articles");
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(JSON.parse(articlesFromLocalStorage));
+    }
+  }, []);
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement | HTMLInputElement>
+  ) => {
     e.preventDefault();
-    const {data} = await getSummary({url: article.url});
+    // check if the article is already in the history
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+    // if it is, display the summary
+    if (existingArticle) return setArticle(existingArticle);
+
+    // if not, fetch the summary and add it to the history
+    const { data } = await getSummary({ url: article.url });
 
     if (!data) return;
 
     if (data.summary) {
-      setArticle({ ...article, summary: data.summary });
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
+    }
+  };
+
+  // copy the url and toggle the icon for user feedback
+  const handleCopy = (url: string) => {
+    setCopied(url);
+    navigator.clipboard.writeText(url);
+    setTimeout(() => setCopied(""), 3000);
   };
 
   return (
@@ -41,6 +86,7 @@ const Demo = () => {
             placeholder="Paste the article link"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
             className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
@@ -53,11 +99,51 @@ const Demo = () => {
         </form>
 
         {/* Browse History */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto"></div>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Result */}
-      <div className="my-10 max-w-full flex justify-center items-center"></div>
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
